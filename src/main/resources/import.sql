0aab506c927bf7cc11eed488f780971c20fc5568
victorvaz001 <victorvaz001@gmail.com>
2023-12-02 11:41:17 -0300

    Seed Bloco

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,6 @@ INSERT INTO tb_categoria (descricao) VALUES ('Oficina');
 INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.00, 1);
 INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Oficina Github', 'Controle de versões de seus projetos', 50.00, 2);
 
+INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z', '2017-09-25T11:00:00Z', 1);
+INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z', '2017-09-25T18:00:00Z', 2);
+INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z', '2017-09-26T11:00:00Z', 2);
