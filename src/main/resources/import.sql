dd7c3197947c324d097e3b31dee8bb264e3f6fb3
victorvaz001 <victorvaz001@gmail.com>
2023-12-02 11:35:19 -0300

    Seed Categoria e Atividade

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 INSERT INTO tb_categoria (descricao) VALUES ('Curso');
 INSERT INTO tb_categoria (descricao) VALUES ('Oficina');
+
+INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.00, 1);
+INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Oficina Github', 'Controle de versões de seus projetos', 50.00, 2);
+
